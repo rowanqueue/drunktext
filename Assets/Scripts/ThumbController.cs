ebc7db6891a6c4db902f36380ec513bd3672e493
Rowan <johnnyjosephwood@gmail.com>
2018-09-28 01:51:46 -0400

    thumbs press keys

--- Assets/Scripts/ThumbController.cs
+++ Assets/Scripts/ThumbController.cs
@@ -13,6 +13,7 @@ public class ThumbController : MonoBehaviour
 	public Vector2 leftXConstraints;
 	public Vector2 leftYConstraints;
 	public bool leftPressing;
+	public float leftDuration;
 	
 	public GameObject thumbR;
 	private Rigidbody rrb;
@@ -22,9 +23,13 @@ public class ThumbController : MonoBehaviour
 	public bool rightPressing;
 
 	public float mod;
+	public float maxSpeed;
+	public float regularZ;
+	public float pressDepth;
 	// Use this for initialization
 	void Start ()
 	{
+		regularZ = thumbL.transform.position.z;
 		lrb = thumbL.GetComponent<Rigidbody>();
 		rrb = thumbR.GetComponent<Rigidbody>();
 	}
@@ -39,11 +44,48 @@ public class ThumbController : MonoBehaviour
 	{
 		lrb.AddForce(leftInput*mod);
 		rrb.AddForce(rightInput*mod);
-		DrunkenShaking(thumbL,lrb,leftXConstraints,leftYConstraints,0.0f);
-		DrunkenShaking(thumbR,rrb,rightXConstraints,rightYConstraints,5.0f);
+		//DrunkenShaking(thumbL,lrb,0.0f);
+		//DrunkenShaking(thumbR,rrb,5.0f);
+		Constrain(thumbL,lrb,leftXConstraints,leftYConstraints);
+		Constrain(thumbR,rrb,rightXConstraints,rightYConstraints);
+		
 		if (leftPressing)
 		{
-			//LERP THAT FUCKER!!
+			if (thumbL.transform.position.z < pressDepth + regularZ)
+			{
+				lrb.AddForce(new Vector3(0,0,1),ForceMode.Impulse);
+			}
+			else
+			{
+				lrb.AddForce(new Vector3(0,0,-1),ForceMode.Impulse);
+				leftPressing = false;
+			}
+		}
+		else
+		{
+			if (thumbL.transform.position.z <= regularZ)
+			{
+				lrb.velocity = new Vector3(lrb.velocity.x,lrb.velocity.y,0);
+			}
+		}
+		if (rightPressing)
+		{
+			if (thumbR.transform.position.z < pressDepth + regularZ)
+			{
+				rrb.AddForce(new Vector3(0,0,1),ForceMode.Impulse);
+			}
+			else
+			{
+				rrb.AddForce(new Vector3(0,0,-1),ForceMode.Impulse);
+				rightPressing = false;
+			}
+		}
+		else
+		{
+			if (thumbR.transform.position.z <= regularZ)
+			{
+				rrb.velocity = new Vector3(rrb.velocity.x,rrb.velocity.y,0);
+			}
 		}
 	}
 
@@ -52,10 +94,10 @@ public class ThumbController : MonoBehaviour
 		//left thumb
 		float x = Input.GetAxis("HorizontalLeft");
 		float y = Input.GetAxis("VerticalLeft");
-		leftInput = new Vector2(x,y).normalized*0.05f;
+		leftInput = new Vector2(x,y).normalized;
 
-		float lPress = Input.GetAxis("FireLeft");
-		if (lPress > 0)
+		//float lPress = Input.GetAxisRaw("FireLeft");
+		if (Input.GetButtonDown("FireLeft"))
 		{
 			if (leftPressing == false)
 			{
@@ -66,24 +108,19 @@ public class ThumbController : MonoBehaviour
 		//right thumb
 		x = Input.GetAxis("HorizontalRight");
 		y = Input.GetAxis("VerticalRight");
-		rightInput = new Vector2(x, y).normalized * 0.05f;
+		rightInput = new Vector2(x, y).normalized;
 
-		float rPress = Input.GetAxis("FireRight");
+		if (Input.GetButtonDown("FireRight"))
+		{
+			if (rightPressing == false)
+			{
+				rightPressing = true;
+			}
+		}
 	}
-	void DrunkenShaking(GameObject thumb,Rigidbody rb, Vector2 xConstraints,Vector2 yConstraints,float offset)
-	{
-		//CHANGE THIS SHIT TO FORCES
-		Vector2 perlin = new Vector2(Mathf.PerlinNoise(Time.time*2f+offset, 0),Mathf.PerlinNoise(0,Time.time*2f+offset));
-		if (perlin.x < 0.5f)
-		{
-			perlin.x = -perlin.x;
-		}
 
-		if (perlin.y < 0.5f)
-		{
-			perlin.y = -perlin.y;
-		}
-		rb.AddForce(perlin.normalized*2f);
+	void Constrain(GameObject thumb, Rigidbody rb, Vector2 xConstraints, Vector2 yConstraints)
+	{
 		if (thumb.transform.position.x < xConstraints.x)//push to the right!
 		{
 			rb.AddForce(Vector2.right,ForceMode.Impulse);
@@ -100,6 +137,22 @@ public class ThumbController : MonoBehaviour
 		{
 			rb.AddForce(Vector2.down,ForceMode.Impulse);
 		}
-		rb.velocity = Vector3.ClampMagnitude(rb.velocity, 0.75f);
+		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+	}
+	void DrunkenShaking(GameObject thumb,Rigidbody rb,float offset)
+	{
+		//CHANGE THIS SHIT TO FORCES
+		Vector2 perlin = new Vector2(Mathf.PerlinNoise(Time.time*2f+offset, 0),Mathf.PerlinNoise(0,Time.time*2f+offset));
+		if (perlin.x < 0.5f)
+		{
+			perlin.x = -perlin.x;
+		}
+
+		if (perlin.y < 0.5f)
+		{
+			perlin.y = -perlin.y;
+		}
+		rb.AddForce(perlin.normalized*2f);
+		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
 	}
 }

2d271f387d62924c1dd27bc64fe37e8f7561f76f
Rowan <johnnyjosephwood@gmail.com>
2018-09-28 01:53:28 -0400

    writer keeps track of written input

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+	public char key;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Thumb")
+		{
+			Writer.me.TapKey(key);
+		}
+	}
+}
---
+++ Assets/Scripts/Writer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Writer : MonoBehaviour
+{
+	public static Writer me;
+
+	public string written;
+	public List<string> archive;
+	public Text display;
+	public Text red;
+	public Text blue;
+	// Use this for initialization
+	void Awake ()
+	{
+		me = this;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		display.text = written;
+		
+	}
+
+	public void Submit()
+	{
+		archive.Add(written);
+		written = "";
+	}
+	public void TapKey(char c)
+	{
+		switch(c)
+		{
+			case '.'://means enter
+				Submit();
+				break;
+			case '\''://means backspace
+				written = written.Substring(0, written.Length - 1);
+				break;
+			default:
+				written += c;
+				break;
+		}
+	}
+}

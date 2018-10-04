194cecf7ffe4f91cf13d89e885e7bc7be30e1ba4
Rowan <johnnyjosephwood@gmail.com>
2018-10-04 15:55:58 -0400

    generates infinite street

---
+++ Assets/Scripts/NextBlockTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextBlockTrigger : MonoBehaviour
+{
+	public float blockLength;//put next block twice this
+	private Transform parent;
+	private bool finished;
+
+	void Start()
+	{
+		parent = transform.parent;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (finished == false)
+		{
+			GameObject block = Instantiate(Resources.Load("Block"), parent.position + new Vector3(0, 0, blockLength * 2f), parent.parent.rotation,
+				transform.parent.parent) as GameObject;
+			block.transform.rotation = parent.parent.rotation;
+			block.transform.localPosition = new Vector3(0,0,parent.localPosition.z+blockLength);
+			finished = true;
+			if (parent.localPosition.z > 295)
+			{
+				Destroy(parent.parent.GetChild(0).gameObject);	
+			}
+		}
+	}
+}

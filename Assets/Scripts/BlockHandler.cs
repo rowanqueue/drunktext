9808895c46f6d07c23fbbb33c14bb57943c6fb85
Rowan <johnnyjosephwood@gmail.com>
2018-10-16 02:54:28 -0400

    better better functioning generation

--- Assets/Scripts/BlockHandler.cs
+++ Assets/Scripts/BlockHandler.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 //handle when to spawn new blocks
 //handle spawning obstacles on these blocks
-public class BlockHandler : MonoBehaviour {
-
-	// Use this for initialization
+public class BlockHandler : MonoBehaviour
+{
+	public int avgnum;//how many obstacles per block
+	public int stdDifference;//how far from avg can it 
+	public GameObject[] prefabs;
+	public int[][] positions;//where you can put the different prefabs
+	public 
+	// Use this for=f initialization
 	void Start () {
 		
 	}
